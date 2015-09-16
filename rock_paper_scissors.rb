f1c4723c32a1b8c39d8adc4fcd00bba021bbfd3c
anne <richardson.ae@gmail.com>
2015-09-16 14:46:42 -0500

    added ascii art for victories

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,5 +1,40 @@
 play = "Y"
 
+def rock
+  my_str = <<-art
+  ___ __  __   _   ___ _  _ _ 
+ / __|  \/  | /_\ / __| || | |
+ \__ \ |\/| |/ _ \\__ \ __ |_|
+ |___/_|  |_/_/ \_\___/_||_(_)
+                                                          
+  art
+  puts my_str
+  puts "Rock smashes Scissors!"
+end
+
+def paper
+  my_str = <<-art
+  ___ __  __   _   ___ _  ___ 
+ / __|  \/  | /_\ / __| |/ / |
+ \__ \ |\/| |/ _ \ (__| ' <|_|
+ |___/_|  |_/_/ \_\___|_|\_(_)                            
+  art
+  puts my_str
+  puts "Paper wraps Rock!"
+end
+
+def scissors
+  my_str = <<-art
+  ___ _      _   ___ _  _ _ 
+ / __| |    /_\ / __| || | |
+ \__ \ |__ / _ \\__ \ __ |_|
+ |___/____/_/ \_\___/_||_(_)                          
+  art
+  puts my_str
+  puts "Scissors cut Paper!"
+end
+
+
 def play_game
  puts "weeee we're playing!"
 
