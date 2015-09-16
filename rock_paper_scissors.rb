78e90391f6f258da4e3650b1195b7ecc4796819f
anne <richardson.ae@gmail.com>
2015-09-16 15:33:21 -0500

    added pseudocode to top

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,5 +1,13 @@
-play = "Y"
+#pseudocode:
+#1. establish available weapons: rock, paper, scissors
+#2. establish win / lose / tie combonations: r > s, s > p, p > r
+#3. get user weapon choice
+#4. get computer weapon choice
+#5. evaluate outcome from user p.o.v.
+#6. announce winner
+#7. offer to play again
 
+play = "Y"
 
 def rock
   my_str = <<-art
@@ -54,20 +62,36 @@ def play_game
     your_choice = "Paper"
   elsif weapon_choice.capitalize.include? "S"
     your_choice = "Scissors"
-  else
+  else #TODO: make proper loop to handle this
     puts "That was not a valid choice, silly."
   end#if
   computer_choice = weapons.sample
   puts "Your #{your_choice.upcase} v.s. my #{computer_choice.upcase}..."
+  sleep(1)
 
-
-
-
-
-
+  if (your_choice == "Rock") && (computer_choice == "Rock")
+    puts "It's a TIE!"
+  elsif (your_choice == "Rock") && (computer_choice == "Paper")
+    puts "You Lose!"
+  elsif (your_choice == "Rock") && (computer_choice == "Scissors")
+    rock
+    puts "You Win!"
+  elsif (your_choice == "Paper") && (computer_choice == "Rock")
+    paper
+    puts "You Win!"  
+  elsif (your_choice == "Paper") && (computer_choice == "Paper")
+    puts "It's a TIE!"
+  elsif (your_choice == "Paper") && (computer_choice == "Scissors")
+    puts "You Lose!"
+  elsif (your_choice == "Scissors") && (computer_choice == "Rock")
+    puts "You Lose!"  
+  elsif (your_choice == "Scissors") && (computer_choice == "Paper")
+    scissors
+    puts "You Win!"
+  elsif (your_choice == "Scissors") && (computer_choice == "Scissors")
+    puts "It's a TIE!"
+  end #if
 end #play_game
-
-
 
 title = "Play Rock, Paper, Scissors!"
 maxlength = title.length.to_i + 6
@@ -82,7 +106,7 @@ puts "Rock beats Scissors"
 puts "Paper beats Rock"
 puts "Scissors beats Paper"
 play_game
-
+puts
 puts "Play Again?  Y / N"
 play = gets.chomp
 
